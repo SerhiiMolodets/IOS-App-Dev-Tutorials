b97a8d342155f4671aaa36e862547de72de98389
SerhiiMolodets <mr.molodets@gmail.com>
2023-01-08 22:36:21 +0200

    added updating data by actions;

--- Scrumdinger/Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Scrumdinger/Views/MeetingView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
     @Binding var scrum: DailyScrum
     @StateObject var scrumTimer = ScrumTimer()
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
     
     var body: some View {
         ZStack {
@@ -29,7 +31,11 @@ struct MeetingView: View {
         .foregroundColor(scrum.theme.accentColor)
         .onAppear {
             scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
-            scrumTimer.stopScrum()
+            scrumTimer.speakerChangedAction = {
+                player.seek(to: .zero)
+                player.play()
+            }
+            scrumTimer.startScrum()
         }
         .onDisappear {
             scrumTimer.stopScrum()
